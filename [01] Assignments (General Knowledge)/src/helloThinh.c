61c1d4b11cb20f9cf15952d0bac63c1539625ea0
WB-coder <63363937+WB-coder@users.noreply.github.com>
2025-02-06 20:58:11 +0700

    Add files via upload

---
+++ [01] Assignments (General Knowledge)/src/helloThinh.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void helloThanh() {
+    printf("Hello from Thinh!\n");
+}
---
+++ [01] Assignments (General Knowledge)/src/main.c
@@ -0,0 +1,7 @@
+#include "hello.h"
+
+int main() {
+    helloThanh();
+    helloWorld();
+    return 0;
+}
